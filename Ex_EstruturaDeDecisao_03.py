8d5cfecfb530c62ac6bf2a820b29f47f4d433078
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-21 14:12:55 -0300

    Update Ex_EstruturaDeDecisao_03.py
    Another way to do the same work.

--- Ex_EstruturaDeDecisao_03.py
+++ Ex_EstruturaDeDecisao_03.py
@@ -4,48 +4,33 @@
 
 l1 = input("Informe a letra que defina seu genero: ")
 
-mulher = 'M'
-homem = 'H'
-mulher_trans = 'MT'
-homem_trans = 'HT'
-travesti = 'T'
-androginia = 'A'
-agenero = 'AG'
-binarismo_de_genero = 'BG'
-crossdresser = 'CD'
-dragqueen = 'DQ'
-dragking = 'DK'
-genero_fluido = 'GF'
-transformista = 'TF'
-transgenero = 'TG'
-
-if (l1 != 'M') and (l1 != 'H') and (l1 != 'MT') and (l1 != 'HT') and (l1 != 'T') and (l1 != 'A') and (l1 != 'AG') and (l1 != 'BG') and (l1 != 'CD') and (l1 != 'DQ') and (l1 != 'DK') and (l1 != 'GF') and (l1 != 'TF') and (l1 != 'TG'):
-    print("Genero ainda nao cadastrado ou invalido!")
-elif l1 == mulher:
-    print("Genero MULHER informado!")
-elif l1 == homem:
-    print("Genero HOMEM informado!")
-elif l1 == mulher_trans:
-    print("Genero MULHER TRANS informado!")
-elif l1 == homem_trans:
-    print("Genero HOMEM TRANS informado!")
-elif l1 == travesti:
-    print("Genero TRAVESTI informado!")
-elif l1 == androginia:
-    print("Genero ANDROGENO informado!")
-elif l1 == agenero:
-    print("Genero AGENERO informado!")
-elif l1 == binarismo_de_genero:
-    print("Genero GENERO BINARIO informado!")
-elif l1 == crossdresser:
-    print("Genero CROSS DRESSER informado!")
-elif l1 == dragqueen:
-    print("Genero DRAG QUEEN informado!")
-elif l1 == dragking:
-    print("Genero DRAG KING informado!")
-elif l1 == genero_fluido:
-    print("Genero GENERO FLUIDO informado!")
-elif l1 == transformista:
-    print("Genero TRANSFORMISTA informado!")
-elif l1 == transgenero:
-    print("Genero TRANSGENERO informado!")
+if l1 == 'M':
+    print ("Genero Mulher informado!")
+elif l1 == 'H':
+    print ("Genero Homem  informado!")
+elif l1 == 'MT':
+    print ("Genero Mulher Trans informado!")
+elif l1 == 'HT':
+    print ("Genero Homem Trans informado!")
+elif l1 == 'T':
+    print ("Genero Travesti informado!")
+elif l1 == 'A':
+    print ("Genero Androginia informado!")
+elif l1 == 'AG':
+    print ("Genero Agenero informado!")
+elif l1 == 'BG':
+    print ("Genero Binarismo De Genero informado!")
+elif l1 == 'CD':
+    print ("Genero Crossdresser informado!")
+elif l1 == 'DQ':
+    print ("Genero Dragqueen informado!")
+elif l1 == 'DK':
+    print ("Genero Dragking informado!")
+elif l1 == 'GF':
+    print ("Genero Genero Fluido informado!")
+elif l1 == 'TF':
+    print ("Genero Transformista informado!")
+elif l1 == 'TG':
+    print ("Genero Transgenero informado!")
+else:
+    print ("Genero ainda nao cadastrado ou invalido!")
